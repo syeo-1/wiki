1a63e937aa4bd4f7503f0c33146a8b4d1eca9f20
Sean Yeo <seansqyeo@gmail.com>
2023-09-23 14:22:08 -0400

    remove dubgging comments

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -14,9 +14,6 @@ def wiki_entry(request, wiki_entry):
     try:
         markdown_file_path = f'entries/{wiki_entry}.md'
         html_from_markdown = markdown2.markdown_path(markdown_file_path)
-        # print(html_stuff)
-        # html_from_markdown = markdown2.markdown(markdown_file_path)
-        # print(html_from_markdown)
         return HttpResponse(html_from_markdown)
     except FileNotFoundError as e:
         return HttpResponse('<p>markdown file not found</p>')

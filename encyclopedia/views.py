d5bf6b07de9a9ca9657ff81b4b3f773ac5140e2e
Sean Yeo <seansqyeo@gmail.com>
2023-09-27 20:30:59 -0400

    use entry html instead of rendering on the spot with httpresponse

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 import markdown2
+from django.urls import reverse
 from django import forms
+import os
 
 from . import util
 
@@ -11,7 +13,7 @@ class NewEntryForm(forms.Form):
 
 def get_entry_html(entry_markdown, entry):
     markdown_to_html = markdown2.markdown(entry_markdown)
-    return HttpResponse(f'<title>{entry}</title>'+markdown_to_html)
+    return f'<title>{entry}</title>'+markdown_to_html
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -23,18 +25,30 @@ def wiki_entry(request, wiki_entry):
     if markdown_page == None:
         return render(request, 'encyclopedia/error.html')
     else:
-        return get_entry_html(markdown_page, wiki_entry)
+        print('test')
+        # return HttpResponse(get_entry_html(markdown_page, wiki_entry))
+        return render(request, 'encyclopedia/entry.html', {
+            'entry_title': wiki_entry,
+            'entry_html': get_entry_html(markdown_page, wiki_entry)
+        })
 
 def search(request):
 
-    query_string = request.GET.get('q')
+    query_string = request.GET.get('q').lower()
     available_entries = [entry.lower() for entry in util.list_entries()]
 
     # check for an exact match for the query string in available entries
+    print(query_string)
+    print(available_entries)
     if query_string in available_entries:
+        print('test')
         entry_to_render = util.list_entries()[available_entries.index(query_string)]
         markdown_page = util.get_entry(entry_to_render)
-        return get_entry_html(markdown_page, entry_to_render)
+        # return HttpResponse(get_entry_html(markdown_page, entry_to_render))
+        return render(request, 'encyclopedia/entry.html', {
+            'entry_title': entry_to_render,
+            'entry_html': get_entry_html(markdown_page, entry_to_render)
+        })
 
 
     # get all entries in which query_string is a substring if no exact match found. returned list may be empty
@@ -44,8 +58,37 @@ def search(request):
     })
 
 def new_entry(request):
-    return render(request, 'encyclopedia/new_page.html', {
-        'form': NewEntryForm()
-    })
+    # on submission what should happen?
+    # it should go to the add endpoint? and then call some function to generate html for the given markdown contents provided
+    # then it should return the html for the markdown
     
+    # at the same time, it should generate a new markdown file within the entries directory
+
+    # upon saving the created markdown file to the entries directory, it should then bring the user to their newly created page!
+
+    ####
+    # for now, try to make sure when a title and entry are given, they are printed to console
+    # on submission, the user will just be brought back to the home page
+    if request.method == 'GET':
+        return render(request, 'encyclopedia/new_page.html', {
+            'form': NewEntryForm()
+        })
+    elif request.method == 'POST':
+        user_title = request.POST.get('new_title')
+        user_entry = request.POST.get('new_markdown_entry')
+
+        # create the markdown file in the entries directory if it doesn't exist
+        if not os.path.exists(f'entries/{user_title}.md'):
+            user_entry = user_entry.replace('\r\n\r\n', '\r\n') # remove double newline that somehow gets added in
+            file = open(f'entries/{user_title}.md', 'w+')
+            file.write(user_entry)
+        else:
+            print('file already exists!')
+            return render(request, 'encyclopedia/new_page.html', {
+                'form': NewEntryForm()
+            })
+
+        markdown = util.get_entry(user_title)
+        return HttpResponse(get_entry_html(markdown, user_title))
+        
 
